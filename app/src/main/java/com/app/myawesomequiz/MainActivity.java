f91d67244ab12760ecd7fc788b8f1c30a91c0e32
Linh Hoang <hoanglinh831999@gmail.com>
2020-04-06 22:11:42 +0700

    add Select difficulty

--- app/src/main/java/com/app/myawesomequiz/MainActivity.java
+++ app/src/main/java/com/app/myawesomequiz/MainActivity.java
@@ -7,16 +7,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_QUIZ = 1;
+    public static final String EXTRA_DIFFICULTY = "extraDifficulty";
 
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String KEY_HIGH_SCORE = "keyHighScore";
 
     private TextView textViewHighScore;
+    private Spinner spinnerDifficulty;
 
     private int highScore;
 
@@ -26,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewHighScore = findViewById(R.id.text_view_highscore);
+        spinnerDifficulty = findViewById(R.id.spinner_difficulty);
+
+        String[] difficultyLevels = Question.getAllDifficultyLevels();
+        ArrayAdapter<String> adapterDifficulty = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, difficultyLevels);
+        adapterDifficulty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDifficulty.setAdapter(adapterDifficulty);
+
         loadHighScore();
 
         Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
@@ -38,7 +51,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startQuiz() {
+        String difficulty = spinnerDifficulty.getSelectedItem().toString();
+
         Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+        intent.putExtra(EXTRA_DIFFICULTY, difficulty);
         startActivityForResult(intent, REQUEST_CODE_QUIZ);
     }
 
